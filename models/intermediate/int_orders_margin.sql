57a1493972c9fa0606b0149d8b57dede94fd7f52
Arno Verdoodt <arno.verdoodt@gmail.com>
2023-10-31 15:35:50 +0000

    int_orders_margin rounded

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,10 @@
 select
     orders_id,
     date_date,
-    sum(revenue) as revenue,
+    ROUND(sum(revenue),2) as revenue,
     sum(quantity) as quantity,
-    sum(purchase_cost) as purchase_cost,
-    sum(margin) as margin
+    ROUND(sum(purchase_cost),2) as purchase_cost,
+   ROUND(sum(margin),2) as margin
 
 from {{ ref("stg_raw__sales") }}
 join {{ ref("int_sales_margin") }} using (products_id)

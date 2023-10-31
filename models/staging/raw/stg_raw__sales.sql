d5eca7bac767eff922b3f486670aebc582d7c80e
Arno Verdoodt <arno.verdoodt@gmail.com>
2023-10-31 14:56:07 +0000

    purchase_cost and margin columns

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -11,9 +11,9 @@ renamed as (
     select
         date_date,
         orders_id,
-        pdt_id AS product_id,
-        revenue,
-        quantity
+        pdt_id AS products_id,
+        CAST(revenue as float64) AS revenue,
+        CAST(quantity as float64) AS quantity
 
     from source
 
